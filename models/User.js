9f6936a2f69d2003ab8bec8184156169d9d5aa74
Allan Mogusu <allannelly690@gmail.com>
2019-03-06 00:06:12 +0300

    Get user profile endpoint

--- models/User.js
+++ models/User.js
@@ -52,4 +52,13 @@ UserSchema.methods.toAuthJSON = function(){
     };
 };
 
+UserSchema.methods.toProfileJSONFor = function(user){
+    return {
+        username: this.username,
+        bio: this.bio,
+        image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg' ,
+        following: false //temporary
+    }
+};
+
 mongoose.model('User', UserSchema);
